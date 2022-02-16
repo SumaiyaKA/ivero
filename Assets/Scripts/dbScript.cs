d233d7a0dc3ca9d0443ad6beb7c62145fac72146
Sumaiya <d17126680@mydit.ie>
2022-02-16 17:37:55 +0000

    Commit 0001: init git repo

---
+++ Assets/Scripts/dbScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+
+public class dbScript : MonoBehaviour
+{
+    private string dbName = "URI=file: testDB.db";
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDB();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateDB()
+    {
+        // connection.open();
+        using (var connection = new SqliteConnection(dbName)){
+
+            connection.Open();
+
+            using (var command = connection.CreateCommand()){
+                command.CommandText = "CREATE TABLE IF NOT EXISTS products ( id_	INTEGER NOT NULL UNIQUE, Name	TEXT NOT NULL, Desc	TEXT NOT NULL, PRIMARY KEY(id_) )";
+                command.ExecuteNonQuery();
+            }
+
+            connection.Close();
+        }
+
+    }
+}
