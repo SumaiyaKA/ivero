a0c93a499cb8a3eb522a32a01a9ee3e2f87b907c
Sumaiya <d17126680@mydit.ie>
2022-04-05 14:54:41 +0100

    Commit0011: final touches

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿// Author: Sumaiya Al Amri D17126680
+// This is gives the developer the ability to create items in..
+// Unity as a prefab with fields like price and image.
+
+// Packages used
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
--- Assets/Scripts/Navigation.cs
+++ Assets/Scripts/Navigation.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿// Author: Sumaiya Al Amri D17126680
+
+
+// Packages used
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
